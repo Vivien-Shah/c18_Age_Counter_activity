678cc45e211512be7c48c33593cb9a5158b57473
Vivien-Shah <hfsthane7787@gmail.com>
2025-09-14 18:14:27 +0530

    Add files via upload

---
+++ c18_Age_Counter_activity.py
@@ -0,0 +1,19 @@
+'''Age Counter
+To write a program to check weather the age entered by the user is correct 
+or not.And to check if the age entered by the user is even or odd
+
+try:
+    age = int(input("Enter your age: "))
+   # print("Your age is correct",age)
+
+    if age%2==0:
+        print("The number is even")
+
+    else:
+        print("The number is odd")
+
+
+except ValueError as ex:
+    print("Exception",ex)'''
+
+
